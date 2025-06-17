0c4ba2f30555328e8bee18f297d3091b992291a4
mujubarrahaman <rahamanmuju@gmail.com>
2025-06-17 20:51:44 +0530

    port 4000

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require ("express")
 const cors = require ("cors")
 const mongoose= require("mongoose")
-const port = process.env.PORT || 5000 
+const port = process.env.PORT || 4000 
 
 const app = express()
 //Install NodeMailer
